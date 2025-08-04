4c12c53563d0dfc30daaa3a6f2797ee2d664c878
olivejua <45840556+olivejua@users.noreply.github.com>
2025-08-04 22:07:57 +0900

    경로함수 의존성 학습

---
+++ chap6/dep_fuc2.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Depends, Query
+
+app = FastAPI()
+
+# 의존성 함수
+def check_dep(name: str = Query(...), gender: str = Query(...)):
+    if not name:
+        raise
+
+# 경로 함수 / 웹 엔드포인트:
+@app.get("/check_user", dependencies=[Depends(check_dep)])
+def get_user() -> bool:
+    return True
